6d30177ac8a59103be71d35c096d3e394ae59769
Syrex72 <kaikcpereira@gmail.com>
2024-08-26 23:26:11 -0300

    ultimo update da noite

--- main.lua
+++ main.lua
@@ -3,19 +3,44 @@ _G.love = require('love')
 
 function love.load()
     love.graphics.setBackgroundColor(100/255, 100/255, 100/255)
-    _G.pacman = {}
-    pacman.x = 250  
-    pacman.y = 250
-    pacman.eat = false
+    _G.pacman = {
+        x = 250,
+        y = 250,
+        angle1 = 1,
+        angle2 = 5
+    }
 
-    _G.food_x = 650
+    _G.food = {
+        x = 650,
+        eaten = false
+    }
 end
 
 function love.update(dt)
-    pacman.x = pacman.x + 1
-    
-    if pacman.x >= food_x then
-        pacman.eat = true
+    if love.keyboard.isDown("down") then
+        pacman.angle2 = pacman.angle2 + math.pi * dt
+        pacman.angle1 = pacman.angle1 + math.pi * dt
+    elseif love.keyboard.isDown("up") then
+        pacman.angle1 = pacman.angle1 - math.pi * dt
+        pacman.angle2 = pacman.angle2 - math.pi * dt
+    end
+
+
+    if love.keyboard.isDown("a") then
+        pacman.x = pacman.x - 10
+    end
+    if love.keyboard.isDown("d") then
+        pacman.x = pacman.x + 10 
+    end
+    if love.keyboard.isDown("w") then
+        pacman.y = pacman.y - 10
+    end
+    if love.keyboard.isDown("s") then
+        pacman.y = pacman.y + 10
+    end 
+
+    if pacman.x >= food.x then
+        food.eaten = true
     end
 end
 
@@ -23,11 +48,11 @@ function love.draw()
     -- love.graphics.setColor(0, 0, 1) -- caso pegue um rgb por fora, dividir por 255 se ele n for entre 0 e 1
     -- love.graphics.rectangle('fill', 0, 0, 100, 100)
     
-    if not pacman.eat then
+    if not food.eaten then
         love.graphics.setColor(0, 1, 0) -- caso pegue um rgb por fora, dividir por 255 se ele n for entre 0 e 1
-        love.graphics.circle('fill', food_x, 250, 50)        
+        love.graphics.circle('fill', food.x, 250, 50)        
     end
 
     love.graphics.setColor(1, 1, 0) -- caso pegue um rgb por fora, dividir por 255 se ele n for entre 0 e 1
-    love.graphics.arc('fill', pacman.x, pacman.y, 100, 1, 5)
+    love.graphics.arc('fill', pacman.x, pacman.y, 100, pacman.angle1, pacman.angle2)
 end

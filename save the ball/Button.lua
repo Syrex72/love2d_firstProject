ceb316df1e1a9d0fe88770bf9684f974d9684597
Syrex72 <kaikcpereira@gmail.com>
2024-08-30 12:32:03 -0300

    att no jogo com agumas logias, verificar problemas com love.mousepressed, não esta funcionando

---
+++ save the ball/Button.lua
@@ -0,0 +1,70 @@
+local love = require 'love'
+
+function Button(text, func, func_param, width, heigth)
+    return{
+        text = text or "No text",
+        width = width or 120,
+        heigth = heigth or 40,
+
+        func = func or function ()
+            print("Empty Button")
+        end,
+        func_param = func_param,
+
+
+        button_x = 0,
+        button_y = 0,
+
+        text_x = 0,
+        text_y = 0,
+        
+        checkPressed = function (self, mouse_x, mouse_y, cursor_radius)
+            if (mouse_x + cursor_radius >= self.button_x) and (mouse_x - cursor_radius <= self.button_x + self.width) then
+                if (mouse_y + cursor_radius >= self.button_y) and (mouse_y - cursor_radius <= self.button_y + self.heigth) then
+                    if self.func_param then
+                        love.graphics.print("clicado 1", 100, 50)
+                        self.func(self.func_param)
+                    else
+                        love.graphics.print("clicado 2", 100, 50)
+                        self.func()
+                    end
+                end
+            end
+        end,
+
+        draw = function (self, button_x, button_y, text_x, text_y)
+            self.button_x = button_x or self.button_x
+            self.button_y = button_y or self.button_y   
+
+
+            if text_x then
+                self.text_x = text_x + self.button_x
+            else
+                self.text_x = self.button_x
+            end
+            if text_y then
+                self.text_y = text_y + self.button_y
+            else
+                self.text_y = self.button_y
+            end
+
+            love.graphics.print("tx:" .. self.text_x .. "ty: " .. self.text_y, 100, 50)
+            love.graphics.print("bx:" .. self.button_x .. "by: " .. self.button_y, 100, 70)
+            
+            love.graphics.setColor(0 , 0, 1)
+            love.graphics.rectangle("fill", self.button_x, self.button_y, self.width, self.heigth)
+
+            love.graphics.setColor(0, 1, 0)
+            love.graphics.print(self.text, self.text_x, self.text_y)
+            -- love.graphics.print(self.text, 0, 0)
+
+
+            
+  
+            love.graphics.setColor(1, 1, 1)
+        end
+    }
+
+end
+
+return Button
--- save the ball/main.lua
+++ save the ball/main.lua
@@ -1,14 +1,15 @@
 local love = require 'love'
 local enemy = require 'Enemy'
+local button = require 'Button'
 
 math.randomseed(os.time())
 
 local game = {
     difficulty = 1,
     state = {
-        menu = false,
+        menu = true,
         pause = false,
-        running = true,
+        running = false,
         ended = false
     }
 }
@@ -20,11 +21,31 @@ local player = {
 }
 
 local enemies = {}
- 
+
+local buttons = {
+    menu_state = {},
+}
+
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if not game.state["running"] then
+        if button == 1 then
+            if game.state["menu"] then
+                for index in ipairs(buttons.menu_state) do
+                    buttons.menu_state[index]:checkPressed(x, y, player.radius)
+                end
+            end
+        end
+    end   
+end
+
 function love.load()
     love.window.setTitle("bolas")
     love.mouse.setVisible(false)
 
+    buttons.menu_state.play_game = button("Play game", nil, nil)
+    buttons.menu_state.settings = button("Settings", nil, nil)
+    buttons.menu_state.exit_game = button("Exit", love.event.quit, nil)
     table.insert(enemies, 1, enemy())
 end
 
@@ -37,6 +58,9 @@ function love.update(dt)
 end
 
 function love.draw()
+    
+    local x, y = love.mouse.getPosition()
+    love.graphics.print("x: ".. x .. " y: " .. y , 100, 100)
     love.graphics.printf(
         "FPS: " .. love.timer.getFPS(), 
         love.graphics.newFont(16),
@@ -44,12 +68,16 @@ function love.draw()
         love.graphics.getWidth()
     )
 
-    if game.state["running"] then 
+    if game.state["runni1ng"] then 
         love.graphics.circle("fill", player.x, player.y, player.radius)
         for i = 1, #enemies do
             enemies[i]:draw()
         end
-    end
+    elseif game.state["menu"] then
+        buttons.menu_state.play_game:draw(love.graphics.getWidth() / 2 - buttons.menu_state.play_game.width / 2, 200, 30, 15)
+        buttons.menu_state.settings:draw(love.graphics.getWidth() / 2 - buttons.menu_state.play_game.width / 2, 245, 30, 15)
+        buttons.menu_state.exit_game:draw(love.graphics.getWidth() / 2 - buttons.menu_state.play_game.width / 2, 290, 30, 15)
+    end 
 
     if not game.state["running"] then 
         love.graphics.circle("fill", player.x, player.y, player.radius / 2)

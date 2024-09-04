ab3f88f3ceac75076dcb9816758fafc41af2deeb
Syrex72 <kaikcpereira@gmail.com>
2024-09-04 14:15:27 -0300

    att pause e ajustes de pontuação, segue faltando as configurações

--- save the ball/main.lua
+++ save the ball/main.lua
@@ -26,11 +26,28 @@ local enemies = {}
 
 local buttons = {
     menu_state = {},
+    ended_state = {},
+    pause_state = {}
+}
+
+local fonts = {
+    medium = {
+        font = love.graphics.newFont(16),
+        size = 16
+    },
+    large = {
+        font = love.graphics.newFont(24),
+        size = 24
+    },
+    gigant = {
+        font = love.graphics.newFont(60),
+        size = 60
+    },
 }
 
 local function changeGameState(state)
     game.state["menu"] = (state == "menu")
-    game.state["pause"] = (state == "paused")
+    game.state["pause"] = (state == "pause")
     game.state["running"] = (state == "running")
     game.state["ended"] = (state == "ended")
 end
@@ -49,9 +66,21 @@ function love.load()
     love.window.setTitle("bolas")
     love.mouse.setVisible(false)
 
+    -- menu padrão
     buttons.menu_state.play_game = button("Play game", startGame, nil)
     buttons.menu_state.settings = button("Settings", nil, nil)
     buttons.menu_state.exit_game = button("Exit", love.event.quit, nil)
+
+    -- menu derrota
+    buttons.ended_state.replay_game = button("Replay game", startGame, nil)
+    buttons.ended_state.menu = button("Menu ", changeGameState, "menu")
+    buttons.ended_state.exit_game = button("Exit", love.event.quit, nil)
+
+    -- menu pausa
+    buttons.pause_state.pause = button("Pause", changeGameState, "pause", 50, 30)
+    buttons.pause_state.return_game = button("Return game", changeGameState, "running")
+    buttons.pause_state.menu = button("Menu", changeGameState, "menu")
+    buttons.pause_state.exit_game = button("Exit", love.event.quit, nil)
 end
 
 function love.update(dt)
@@ -69,7 +98,7 @@ function love.update(dt)
                     end
                 end
             else
-                changeGameState("menu")
+                changeGameState("ended")
             end
             enemies[i]:move(player.x, player.y)
         end
@@ -84,12 +113,26 @@ function love.mousepressed(x, y, button, istouch, presses)
                 for index in pairs(buttons.menu_state) do
                     buttons.menu_state[index]:checkPressed(x, y, player.radius)
                 end
+            elseif game.state["ended"] then
+                for index in pairs(buttons.ended_state) do
+                    buttons.ended_state[index]:checkPressed(x, y, player.radius)
+                end
+            elseif game.state["paused"] then
+                for index in pairs(buttons.pause_state) do
+                    buttons.pause_state[index]:checkPressed(x, y, player.radius)
+                end
             end
         end
     end
-end
+    if game.state["running"] then
+        if button == 1 then
+            buttons.pause_state.pause:checkPressed(x, y, player.radius)
+        end
+    end
+end 
 
 function love.draw()
+    love.graphics.setFont(fonts.medium.font)
     love.graphics.printf(
         "FPS: " .. love.timer.getFPS(), 
         love.graphics.newFont(16),
@@ -97,17 +140,26 @@ function love.draw()
         love.graphics.getWidth()
     )
 
-    if game.state["running"] then 
-        love.graphics.print(math.floor(player.points), love.graphics.newFont(24), love.graphics.getWidth() / 2, 0)
+    if game.state["running"] then
+        buttons.pause_state.pause:draw(love.graphics.getWidth() - buttons.pause_state.pause.width, 0)
+        love.graphics.print(math.floor(player.points), fonts.large.font , love.graphics.getWidth() / 2, 0)
         love.graphics.circle("fill", player.x, player.y, player.radius)
         for i = 1, #enemies do
             enemies[i]:draw()
         end
     elseif game.state["menu"] then
         buttons.menu_state.play_game:draw(love.graphics.getWidth() / 2 - buttons.menu_state.play_game.width / 2, 200, 30, 15)
-        buttons.menu_state.settings:draw(love.graphics.getWidth() / 2 - buttons.menu_state.play_game.width / 2, 245, 30, 15)
-        buttons.menu_state.exit_game:draw(love.graphics.getWidth() / 2 - buttons.menu_state.play_game.width / 2, 290, 30, 15)
-    end 
+        buttons.menu_state.settings:draw(love.graphics.getWidth() / 2 - buttons.menu_state.settings.width / 2, 245, 30, 15)
+        buttons.menu_state.exit_game:draw(love.graphics.getWidth() / 2 - buttons.menu_state.exit_game.width / 2, 290, 30, 15)
+    elseif game.state["ended"] then 
+        buttons.ended_state.replay_game:draw(love.graphics.getWidth() / 2 - buttons.ended_state.replay_game.width / 2, 245, 30, 15)
+        buttons.ended_state.menu:draw(love.graphics.getWidth() / 2 - buttons.ended_state.menu.width / 2, 290, 30, 15)
+        buttons.ended_state.exit_game:draw(love.graphics.getWidth() / 2 - buttons.ended_state.exit_game.width / 2, 335, 30, 15)
+    elseif game.state["pause"] then 
+        buttons.pause_state.return_game:draw(love.graphics.getWidth() / 2 - buttons.ended_state.replay_game.width / 2, 245, 30, 15)
+        buttons.pause_state.menu:draw(love.graphics.getWidth() / 2 - buttons.ended_state.menu.width / 2, 290, 30, 15)
+        buttons.pause_state.exit_game:draw(love.graphics.getWidth() / 2 - buttons.ended_state.exit_game.width / 2, 335, 30, 15)
+    end
 
     if not game.state["running"] then 
         love.graphics.circle("fill", player.x, player.y, player.radius / 2)
